e92db3aa00ff94d2a8939ecf246dea7121cb7c73
zs <Z>
2020-10-19 17:07:49 +0300

    Tema Laborator 4

---
+++ GloriaStanciu/L04/Test.App/ProgramQuestionClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+namespace Test.App
+{
+    class ProgramQuestionClass
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new CreateQuestionCmd("Titlu1", "Descriere1", "c#");
+            var result = CreateQuestion(cmd);
+            result.Match(
+                ProcessQuestionCraeted,
+                ProcessQuestionNotCreated,
+                ProcessInvalidQuestion
+                );
+            Console.ReadLine();
+        }
+        private static ICreatedQuestionResult ProcessInvalidQuestion(QuestionValidationFailed validationErrors)
+        {
+            Console.WriteLine("Question validation failed: ");
+            foreach(var error in validationErrors.ValidationErrors)
+            {
+                Console.WriteLine(error);
+            }
+            return validationErrors;
+        }
+        private static ICreatedQuestionResult ProcessQuestionNotCreated(QuestionNotCreated questionNotCreatedResult)
+        {
+            Console.WriteLine($"Question not created: {questionNotCreatedResult.Reason}");
+            return questionNotCreatedResult;
+        }
+        private static ICreatedQuestionResult ProcessQuestionCreated(QuestionCreated question)
+        {
+            Console.WriteLine($"Question {question.QuestionId}");
+            return question;
+        }
+        public static ICreatedQuestionResult CreateQuestion(CreateQuestionCmd createQuestionCommand)
+        {
+            if(string.IsNullOrWhiteSpace(createQuestionCommand.Title) || string.IsNullOrWhiteSpace(createQuestionCommand(Description))) 
+            {
+                var errors = new List<string>() { "Invalid title or description" };
+                return new QuestionValidationFailed(errors);
+            }
+            if(new Random().Next(10)>1)
+            {
+                return new QuestionNotCreated("Question could not be verified");
+            }
+            var questionId = Guid.NewGuid();
+            var results = new QuestionCreated(questionId, createQuestionCommand.Title, createQuestionCommand.Description);
+            return results;
+        }
+    }
+
+}
