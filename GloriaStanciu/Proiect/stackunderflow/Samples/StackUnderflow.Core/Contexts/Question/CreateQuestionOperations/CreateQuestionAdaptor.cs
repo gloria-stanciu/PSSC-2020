8eb547e8b70b2f7eef79e6d4e8844fe30d24c36f
zs <Z>
2020-11-22 18:29:17 +0200

    Create - add question workflow

---
+++ GloriaStanciu/Proiect/stackunderflow/Samples/StackUnderflow.Core/Contexts/Question/CreateQuestionOperations/CreateQuestionAdaptor.cs
@@ -0,0 +1,41 @@
+﻿using Access.Primitives.Extensions.ObjectExtensions;
+using Access.Primitives.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static StackUnderflow.Domain.Core.Contexts.Question.CreateQuestionOperations.CreateQuestionResult;
+
+namespace StackUnderflow.Domain.Core.Contexts.Question.CreateQuestionOperations
+{
+    class CreateQuestionAdaptor : Adapter<CreateQuestionCmd, ICreateQuestionResult, QuestionWriteContext, QuestionDependencies>
+    {
+        public override Task PostConditions(CreateQuestionCmd cmd, ICreateQuestionResult result, QuestionWriteContext state)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async override Task<ICreateQuestionResult> Work(CreateQuestionCmd cmd, QuestionWriteContext state, QuestionDependencies dependencies)
+        {
+            var workflow = from valid in cmd.TryValidate()
+                           let t = AddQuestion(state, CreateQuestionFromCmd(cmd))
+                           select t;
+            var result = await workflow.Match(
+                Succ: r => r,
+                Fail: er => new QuestionNotCreated(er.Message)
+                );
+
+            return result;
+        }
+
+        private ICreateQuestionResult AddQuestion(QuestionWriteContext state, object v)
+        {
+            return new QuestionCreated(new Guid("1"), "Titlu", "Descriere", "Tag-uri");
+        }
+
+        private object CreateQuestionFromCmd(CreateQuestionCmd cmd)
+        {
+            return new { };
+        }
+    }
+}
